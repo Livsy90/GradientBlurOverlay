9ec3022a54f44d63146001a521f523da171ac383
Artem <60046989+Livsy90@users.noreply.github.com>
2025-08-21 14:51:10 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,25 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "GradientBlurOverlay",
+    name: "BottomBlurredOverlay",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "GradientBlurOverlay",
-            targets: ["GradientBlurOverlay"]
+            name: "BottomBlurredOverlay",
+            targets: ["BottomBlurredOverlay"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GradientBlurOverlay"
+            name: "BottomBlurredOverlay"
         ),
 
     ]
