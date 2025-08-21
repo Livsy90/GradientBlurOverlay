2bc739a3001fdffec409578ab836a8574c3028cf
Artem <60046989+Livsy90@users.noreply.github.com>
2025-08-21 19:31:45 +0400

    Update GradientBlurOverlay.swift

--- Sources/GradientBlurOverlay/GradientBlurOverlay.swift
+++ Sources/GradientBlurOverlay/GradientBlurOverlay.swift
@@ -1,14 +1,69 @@
-import SwiftUI
-
 public extension View {
-    func gradientBlurOverlay<Overlay: View>(
+    func materialOverlay<Overlay: View>(
         @ViewBuilder overlay: @escaping () -> Overlay
     ) -> some View {
-        modifier(BlurredOverlay(overlay: overlay))
+        modifier(MaterialOverlay(overlay: overlay))
+    }
+    
+    func blurOverlay<Overlay: View>(
+        @ViewBuilder overlay: @escaping () -> Overlay
+    ) -> some View {
+        modifier(BlurOverlay(overlay: overlay))
     }
 }
 
-private struct BlurredOverlay<Overlay: View>: ViewModifier {
+private enum Constants {
+    static let additionalPadding: CGFloat = 20
+    static let blurRadius: CGFloat = 12
+    static let scaleValue: CGFloat = 1.045
+}
+
+private struct BlurOverlay<Overlay: View>: ViewModifier {
+    let overlay: () -> Overlay
+    @State private var height: CGFloat = 300
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottom) {
+            content
+                .overlay {
+                    content
+                        .allowsHitTesting(false)
+                        .blur(radius: Constants.blurRadius)
+                        .scaleEffect(Constants.scaleValue)
+                        .mask {
+                            VStack {
+                                Spacer()
+                                LinearGradient(
+                                    colors: [
+                                        .clear,
+                                        .black.opacity(0.1),
+                                        .black.opacity(0.7),
+                                        .black.opacity(0.9),
+                                        .black,
+                                        .black,
+                                        .black,
+                                        .black
+                                    ],
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                                .frame(height: height + Constants.additionalPadding)
+                            }
+                        }
+                }
+            
+            
+            overlay()
+                .onGeometryChange(for: CGSize.self) { geometry in
+                    geometry.size
+                } action: { newValue in
+                    height = newValue.height
+                }
+        }
+    }
+}
+
+private struct MaterialOverlay<Overlay: View>: ViewModifier {
     let overlay: () -> Overlay
     @State private var height: CGFloat = 300
     
@@ -18,6 +73,7 @@ private struct BlurredOverlay<Overlay: View>: ViewModifier {
             
             Rectangle()
                 .fill(.ultraThinMaterial)
+                .allowsHitTesting(false)
                 .mask {
                     LinearGradient(
                         colors: [
