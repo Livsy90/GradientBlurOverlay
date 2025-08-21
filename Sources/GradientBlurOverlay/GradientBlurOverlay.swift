fcc548088c6a4221849b61ec801380029af3c522
Artem <60046989+Livsy90@users.noreply.github.com>
2025-08-21 14:50:52 +0400

    Update GradientBlurOverlay.swift

--- Sources/GradientBlurOverlay/GradientBlurOverlay.swift
+++ Sources/GradientBlurOverlay/GradientBlurOverlay.swift
@@ -1,2 +1,43 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public extension View {
+    func bottomBlurredOverlay<Overlay: View>(
+        @ViewBuilder overlay: @escaping () -> Overlay
+    ) -> some View {
+        modifier(BlurredOverlay(overlay: overlay))
+    }
+}
+
+private struct BlurredOverlay<Overlay: View>: ViewModifier {
+    let overlay: () -> Overlay
+    @State private var height: CGFloat = 300
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottom) {
+            content
+            
+            Rectangle()
+                .fill(.ultraThinMaterial)
+                .mask {
+                    LinearGradient(
+                        colors: [
+                            .clear,
+                            .black.opacity(0.7),
+                            .black,
+                            .black
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                }
+                .frame(height: height)
+            
+            overlay()
+                .onGeometryChange(for: CGSize.self) { geometry in
+                    geometry.size
+                } action: { newValue in
+                    height = newValue.height
+                }
+        }
+    }
+}
